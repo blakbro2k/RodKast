c067f04f66ac3d0ee228f23046ea2e18c6071af6
eboateng <boaten01@gmail.com>
2017-10-09 17:39:48 -0400

    added testing

---
+++ core/test/net/asg/game/utils/parser/XMLEnclosureTest.java
@@ -0,0 +1,14 @@
+package net.asg.game.utils.parser;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * Created by eboateng on 10/9/2017.
+ */
+
+public class XMLEnclosureTest {
+}
