f5388b986640241e594ca87f447e3b445d0b3def
Enoch Boateng <boaten01@gmail.com>
2017-10-10 07:39:16 -0400

    added RodkastEpisodeTest

--- core/test/net/asg/game/utils/parser/XMLEnclosureTest.java
+++ core/test/net/asg/game/utils/parser/XMLEnclosureTest.java
@@ -1,9 +1,9 @@
 package net.asg.game.utils.parser;
 
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 /**
@@ -11,4 +11,49 @@ import java.net.URL;
  */
 
 public class XMLEnclosureTest {
+
+    @Test
+    public void getLength() throws Exception {
+        XMLEnclosure zeforLength = new XMLEnclosure(0,"mpge","http://rodkast.com/feed/podcast");
+        XMLEnclosure nonZeforLength = new XMLEnclosure(10,"mpge","http://rodkast.com/feed/podcast");
+
+
+        try{
+            XMLEnclosure negLength = new XMLEnclosure(-1,"mpge","http://rodkast.com/feed/podcast");
+        } catch (Exception ille){
+            Assert.assertTrue(ille instanceof IllegalArgumentException);
+        }
+
+        Assert.assertEquals(0f,zeforLength.getLength(),0);
+        Assert.assertEquals(10f,nonZeforLength.getLength(),0);
+    }
+
+    @Test
+    public void getType() throws Exception {
+        XMLEnclosure goodType = new XMLEnclosure(10,"mpge","http://rodkast.com/feed/podcast");
+
+        try{
+            XMLEnclosure nullType = new XMLEnclosure(-1,null,"http://rodkast.com/feed/podcast");
+        } catch (Exception ille){
+            Assert.assertTrue(ille instanceof IllegalArgumentException);
+        }
+
+        Assert.assertEquals("mpge",goodType.getType());
+    }
+
+    @Test
+    public void getUrl() throws Exception {
+        XMLEnclosure goodType = new XMLEnclosure(10,"mpge","http://rodkast.com/feed/podcast");
+        URL expectedURL = new URL("http://rodkast.com/feed/podcast");
+        //XMLEnclosure nonZeforLength = new XMLEnclosure(10,"mpge","http://rodkast.com/feed/podcast");
+
+        try{
+            XMLEnclosure nullType = new XMLEnclosure(11,"mpge",null);
+        } catch (Exception ille){
+            Assert.assertTrue(ille instanceof MalformedURLException);
+        }
+
+        Assert.assertEquals(expectedURL,goodType.getUrl());
+    }
 }
+
